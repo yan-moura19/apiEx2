b3a4a67a3d9980e6ffd1b062ac2f2cd11c609a99
yan-moura19 <yangabriel3301@gmail.com>
2023-04-03 23:41:59 -0300

    Adicionar arquivos de projeto.

---
+++ apiEx2/Controllers/NoticiaController.cs
@@ -0,0 +1,70 @@
+﻿using apiEx2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiEx2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly List<Noticia> noticias = new List<Noticia>();
+
+        public NoticiaController() {
+            
+            
+            noticias.Add(new Noticia {Autor = "Yan", Conteudo = "teste 1, teste 2, teste 3 ", Data = "03/04/2023",Id = 1,Titulo = "testando1"});
+            noticias.Add(new Noticia { Autor = "João", Conteudo = "teste 2, teste 3, teste 4 ", Data = "03/03/2023", Id = 2, Titulo = "testando2" });
+            noticias.Add(new Noticia { Autor = "Pedro", Conteudo = "teste 5, teste 6, teste 7 ", Data = "03/02/2023", Id = 3, Titulo = "testando3" });
+        }
+
+        [HttpGet]
+        public List<Noticia> GetAll() {
+            return noticias;
+        }
+        
+        [HttpPost]
+        public IActionResult Add([FromBody] Noticia noticia)
+        {
+            noticia.Id = noticia.Id;
+            noticias.Add(noticia);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByID(string id)
+        {
+            if (id == null) return NotFound();
+
+            var noticia = noticias.Where(noticia => noticia.Id.Equals(id)).FirstOrDefault();
+            return Ok(noticia);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            if (id == null) return NotFound();
+            Noticia? noticia = GetNoticia(id);
+
+            if (noticia == null) return NotFound();
+
+            noticias.Remove(noticia);
+
+            return Ok("noticia removida com sucesso!");
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, [FromBody] Noticia noticia)
+        {
+            var editNoticia = GetNoticia(id);
+            if (editNoticia == null) return NoContent();
+
+            editNoticia.Titulo = noticia.Titulo;
+            editNoticia.Autor = noticia.Autor;
+            editNoticia.Data = noticia.Data;
+            editNoticia.Conteudo = noticia.Conteudo.ToString();
+
+            return Ok(editNoticia);
+        }
+        private Noticia? GetNoticia(string id)
+        {
+            return noticias.Where(noticia => noticia.Id.ToString().Equals(id)).FirstOrDefault();
+        }
+    }
+}
---
+++ apiEx2/Model/Noticia.cs
@@ -0,0 +1,12 @@
+﻿namespace apiEx2.Model
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Autor { get; set; }
+        public string Data { get; set; }
+    }
+}
